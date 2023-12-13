689e246f8cd320d215fcba66a3d2f13199773108
Madhav Bikram GC <mbnepali15@gmail.com>
2023-12-13 17:18:21 +0545

    answering

--- routes/organizationRoute.js
+++ routes/organizationRoute.js
@@ -1,17 +1,20 @@
-const { renderOrganizationForm, createOrganization, createQuestionsTable, createAnswersTable, renderDashboard, renderForumPage, renderQuestionForm, createQuestion, renderSingleQuestion, createQuestionImages } = require("../controller/organization/organizationController")
+const { renderOrganizationForm, createOrganization, createQuestionsTable, createAnswersTable, renderDashboard, renderForumPage, renderQuestionForm, createQuestion, renderSingleQuestion, createQuestionImages, answerQuestion } = require("../controller/organization/organizationController")
 const { isAuthenticated } = require("../middleware/isAuthenticated")
 const {multer,storage} = require('../middleware/multerConfig')
 const upload = multer({storage : storage})
 
 const router = require("express").Router()
 
-router.route("/organization").get(renderOrganizationForm).post(isAuthenticated, createOrganization , createQuestionsTable,createQuestionImages, createAnswersTable)
+router.route("/organization").get(renderOrganizationForm).post(isAuthenticated, createOrganization, createQuestionsTable, createQuestionImages, createAnswersTable)
+
 router.route("/dashboard").get(isAuthenticated, renderDashboard)
 
-router.route("/forum").get(isAuthenticated,renderForumPage)
+router.route("/forum").get(isAuthenticated, renderForumPage)
+
 router.route("/question").get(isAuthenticated,renderQuestionForm).post(isAuthenticated,upload.array('questionImage'),createQuestion)
 
 router.route("/question/:id").get(isAuthenticated,renderSingleQuestion)
 
+router.route('/answer').post(isAuthenticated, answerQuestion)
 
 module.exports = router
